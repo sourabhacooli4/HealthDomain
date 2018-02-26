f41c317e784beb4498b38e64f99b2bc0e37e24ec
sourabhacooli4 <sourabhacooli4@gmail.com>
2018-02-26 09:19:32 +0530

    detail
    folder

---
+++ Health/Health/ScriptsNg/Controller/ContactCtrl.js
@@ -0,0 +1,114 @@
+﻿
+app.controller('ContactCtrl', ['$scope', 'CrudService', '$http',
+    function ($scope, CrudService, $http) {
+
+        // Base Url
+        var baseUrl = '/api/Contact/';
+        $scope.btnText = "Save";
+        $scope.id = 0;
+
+        //Insert and Update operation
+        $scope.SaveUpdate = function () {
+            var contact = {
+                FirstName: $scope.firstName,
+                LastName: $scope.lasttName,
+                Email: $scope.email,
+                Phone: $scope.phone,
+                Status: $scope.status,
+                ID: $scope.id
+            }
+            if ($scope.btnText == "Save") {
+                var apiRoute = baseUrl + 'SaveContact/';
+                var saveContact = CrudService.post(apiRoute, contact);
+                saveContact.then(function (response) {
+                    if (response.data != "") {
+                        alert("Data Save Successfully");
+                        $scope.GetContacts();
+                        $scope.Clear();
+
+                    } else {
+                        alert("Some error");
+                    }
+
+                }, function (error) {
+                    console.log("Error: " + error);
+                });
+            }
+            else {
+                var apiRoute = baseUrl + 'UpdateContact/';
+                var UpdateContact = CrudService.put(apiRoute, contact);
+                UpdateContact.then(function (response) {
+                    if (response.data != "") {
+                        alert("Data Update Successfully");
+                        $scope.GetContacts();
+                        $scope.Clear();
+
+                    } else {
+                        alert("Some error");
+                    }
+
+                }, function (error) {
+                    console.log("Error: " + error);
+                });
+            }
+        }
+
+        //To collect all the data
+        $scope.GetContacts = function () {
+            var apiRoute = baseUrl + 'GetContacts/';
+            var contact = CrudService.getAll(apiRoute);
+            contact.then(function (response) {
+                $scope.contactss = response.data;
+            },
+                function (error) {
+                    console.log("Error: " + error);
+                });
+        }
+
+        $scope.GetContacts();
+
+
+        //To get the particular data
+        $scope.GetContactByID = function (dataModel) {
+            $scope.id = dataModel.ID;
+            $scope.firstName = dataModel.FirstName;
+            $scope.lasttName = dataModel.LastName;
+            $scope.email = dataModel.Email;
+            $scope.phone = dataModel.Phone;
+            $scope.status = dataModel.Status;
+            $scope.btnText = "Update";
+        };
+
+
+        //Delete operation
+        $scope.DeleteContact = function (dataModel) {
+            var apiRoute = baseUrl + 'DeleteContact/ID?' + dataModel.ID;
+            var deleteContact = CrudService.delete(apiRoute);
+            deleteContact.then(function (response) {
+                if (response.data != "") {
+                    alert("Data Delete Successfully");
+                    $scope.GetContacts();
+                    $scope.Clear();
+
+                } else {
+                    alert("Some error");
+                }
+
+            }, function (error) {
+                console.log("Error: " + error);
+            });
+        }
+
+
+        //Clear operation
+        $scope.Clear = function () {
+            $scope.ID = 0;
+            $scope.firstName = "";
+            $scope.lasttName = "";
+            $scope.email = "";
+            $scope.phone = "";
+            $scope.status = "";
+            $scope.btnText = "Save";
+        }
+
+    }]);
---
+++ Health/Health/api/ContactController.cs
@@ -0,0 +1,116 @@
+﻿using Health.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace Health.api
+{
+    [RoutePrefix("/api/Contact")]
+    public class ContactController : ApiController
+    {
+        HealthCareEntities dbContext = null;
+
+        public ContactController()
+        {
+            // create instance of an object
+            dbContext = new HealthCareEntities();
+        }
+
+
+        #region SaveContact defination
+        [ResponseType(typeof(tblcontact))]
+        [HttpPost]
+        public HttpResponseMessage SaveContact(tblcontact acontact)
+        {
+            int result = 0;
+            try
+            {
+                dbContext.tblcontacts.Add(acontact);
+                dbContext.SaveChanges();
+                result = 1;
+            }
+            catch (Exception e)
+            {
+
+                result = 0;
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+        #endregion
+
+        #region UpdateContact defination
+        [ResponseType(typeof(tblcontact))]
+        [HttpPut]
+        public HttpResponseMessage UpdateContact(tblcontact acontact)
+        {
+            int result = 0;
+            try
+            {
+                dbContext.tblcontacts.Attach(acontact);
+                dbContext.Entry(acontact).State = EntityState.Modified;
+                dbContext.SaveChanges();
+                result = 1;
+            }
+            catch (Exception e)
+            {
+
+                result = 0;
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+        #endregion
+
+        #region DeleteContact defination
+        [ResponseType(typeof(tblcontact))]
+        [HttpDelete]
+        public HttpResponseMessage DeleteContact(int ID)
+        {
+            int result = 0;
+            try
+            {
+                var contact = dbContext.tblcontacts.Where(x => x.ID == ID).FirstOrDefault();
+                dbContext.tblcontacts.Attach(contact);
+                dbContext.tblcontacts.Remove(contact);
+                dbContext.SaveChanges();
+                result = 1;
+            }
+            catch (Exception e)
+            {
+
+                result = 0;
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+        #endregion
+
+        #region GetContacts defination
+        [ResponseType(typeof(tblcontact))]
+        [HttpGet]
+        public List<tblcontact> GetContacts()
+        {
+            List<tblcontact> contacts = null;
+            try
+            {
+                contacts = dbContext.tblcontacts.ToList();
+            }
+            catch (Exception e)
+            {
+                contacts = null;
+            }
+
+            return contacts;
+        }
+        #endregion
+    }
+}
